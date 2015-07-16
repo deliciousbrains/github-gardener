5143e5305eeba92f1eed4a1c4076f1106c6a0395
Iain <iain@polevaultweb.com>
2015-07-16 20:53:20 +0100

    Make sure the mergeable flag is explicitly false before going ahead

--- include/class-github-gardening.php
+++ include/class-github-gardening.php
@@ -286,7 +286,7 @@ class GitHubGardening {
 
 		$pull_request = $this->client->pulls->getSinglePullRequest( $this->owner, $this->repo, $id );
 
-		if ( $pull_request->isMergeable() ) {
+		if ( false !== $pull_request->isMergeable() ) {
 			if ( $label_exists ) {
 				// TODO remove label
 			}
